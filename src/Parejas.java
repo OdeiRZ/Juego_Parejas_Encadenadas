e9b0269449b22ec53acec464a3b88ecc74dc4280
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-15 15:57:07 +0200

    Agregadas variables globales a usar

--- src/Parejas.java
+++ src/Parejas.java
@@ -2,3 +2,27 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
+        
+public class Parejas extends JFrame implements ActionListener {
+    
+    private final boolean debug = false;
+    
+    private final int columnas = 4;
+    
+    private final int filas = 5;   
+    
+    private final ImageIcon[] imagenes;
+    
+    private final ImageIcon abajo;
+    
+    private final JLabel Lintentos;
+    
+    private final JLabel Puntos;
+           
+    private int ficha[][] = new int[columnas][filas], comprobar = 0, pos1, i1, j1, j2, i2, quedan, intentos = 0;
+    
+    private final JButton boton[][];
+    
+    private static final String ruta = "recursos/";    
+   
+}
